e269df9af4c31ae383697ba62f261882f7f897f1
Daniel Koch <dk.prjects.manager@gmail.com>
2019-01-08 11:13:45 +0100

    now detecting changes at log file

--- logwatcher/logwatcher.go
+++ logwatcher/logwatcher.go
@@ -4,36 +4,89 @@ import (
   "fmt"
   "path/filepath"
   "os"
+  "io"
+  "bytes"
   "github.com/hpcloud/tail"
 )
 
 type Logs map[string]Log
 
 type Log struct {
-  path string
+  lines int
 }
 
 func main() {
-  path := "/home/daniel/Public/LogTests/one"
+  path := "/home/daniel/Public/"
+  //initial files getting
   files := getFiles(path)
 
-  for _,file := range files {
-    fmt.Println(file.path)
-    notify(file.path)
+  for path,file := range files {
+    fmt.Println(path, file.lines)
+  }
+
+  //watch them
+  for {
+    for path,file := range files {
+
+      reader, err := os.Open(path)
+
+      check(err)
+
+      lines, err := lineCounter(reader)
+
+      check(err)
+
+      if (lines > file.lines || lines < file.lines) {
+
+        fmt.Println("___________________________")
+        fmt.Println("changes detected")
+        fmt.Println(path, file.lines)
+        fmt.Println("now",lines)
+        fmt.Println("___________________________")
+
+        log := Log{lines: lines}
+        files[path] = log
+      }
+
+
+    }
   }
 }
 
-func notify(path string) {
+func check(e error) {
+  if e != nil {
+      panic(e)
+  }
+}
+
+func show(path string) {
   t, err := tail.TailFile(path, tail.Config{Follow: true,ReOpen:true})
 
-  if (err != nil) {
-    fmt.Println("error", err)
-  }
+  check(err)
 
   for line := range t.Lines {
       fmt.Println(line.Text)
   }
   fmt.Println(t.Lines,path)
+}
+
+func lineCounter(r io.Reader) (int, error) {
+  buf := make([]byte, 32*1024)
+  count := 0
+  lineSep := []byte{'\n'}
+
+  for {
+      c, err := r.Read(buf)
+      count += bytes.Count(buf[:c], lineSep)
+
+      switch {
+      case err == io.EOF:
+          return count, nil
+
+      case err != nil:
+          return count, err
+      }
+  }
 }
 
 func getFiles(root string) Logs {
@@ -55,7 +108,15 @@ func getFiles(root string) Logs {
       }
 
       if (filepath.Ext(path) == ".log") {
-        log := Log{path: path}
+        reader, err := os.Open(path)
+
+        check(err)
+
+        lines, err := lineCounter(reader)
+
+        check(err)
+
+        log := Log{lines: lines}
         logs[ident] = log
       }
       return nil
