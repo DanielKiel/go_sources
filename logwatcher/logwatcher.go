a54ba65e1428bf7984c7afe3aa0d8297fa707213
Daniel Koch <dk.prjects.manager@gmail.com>
2019-01-09 20:17:19 +0100

    wrote package file filemanager to make main file not so overloaded

--- logwatcher/logwatcher.go
+++ logwatcher/logwatcher.go
@@ -2,26 +2,24 @@ package main
 
 import (
   "fmt"
-  "path/filepath"
   "os"
-  "io"
-  "bytes"
-  "bufio"
+  fm "logwatcher/filemanager"
+  "github.com/subosito/gotenv"
 )
 
-type Logs map[string]Log
 
-type Log struct {
-  lines int
-}
 
 func main() {
+  gotenv.Load()
+
+  //fmt.Println(os.Getenv("SSH_USERNAME"))
+
   path := "/home/daniel/Public/"
   //initial files getting
-  files := getFiles(path)
+  files := fm.GetFiles(path)
 
   for path,file := range files {
-    fmt.Println(path, file.lines)
+    fmt.Println(path, file.Lines)
   }
 
   //watch them
@@ -32,22 +30,22 @@ func main() {
 
       check(err)
 
-      lines, err := lineCounter(reader)
+      lines, err := fm.LineCounter(reader)
 
       check(err)
 
-      if (lines > file.lines || lines < file.lines) {
+      if (lines > file.Lines || lines < file.Lines) {
 
         fmt.Println("___________________________")
         fmt.Println("changes detected")
-        fmt.Println(path, file.lines)
+        fmt.Println(path, file.Lines)
         fmt.Println("now",lines)
         fmt.Println("___________________________")
 
-        log := Log{lines: lines}
+        log := fm.Log{Lines: lines}
         files[path] = log
 
-        show(path)
+        //show(path)
       }
     }
   }
@@ -57,76 +55,4 @@ func check(e error) {
   if e != nil {
       panic(e)
   }
-}
-
-func show(path string) {
-  file, err := os.Open(path)
-
-  check(err)
-
-  defer file.Close()
-
-  scanner := bufio.NewScanner(file)
-
-  for scanner.Scan() {
-    fmt.Println(scanner.Text())
-  }
-}
-
-func lineCounter(r io.Reader) (int, error) {
-  buf := make([]byte, 32*1024)
-  count := 0
-  lineSep := []byte{'\n'}
-
-  for {
-      c, err := r.Read(buf)
-      count += bytes.Count(buf[:c], lineSep)
-
-      switch {
-      case err == io.EOF:
-          return count, nil
-
-      case err != nil:
-          return count, err
-      }
-  }
-}
-
-func getFiles(root string) Logs {
-  logs := make(Logs)
-
-  err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-      //fmt.Println(filepath.Dir(path))
-      ident, miss := filepath.Abs(path)
-
-      if (miss != nil) {
-        fmt.Println(miss)
-      }
-
-      //check if the key already exists
-      _,exists := logs[ident]
-
-      if (exists) {
-        fmt.Println("we must handle the error here")
-      }
-
-      if (filepath.Ext(path) == ".log") {
-        reader, err := os.Open(path)
-
-        check(err)
-
-        lines, err := lineCounter(reader)
-
-        check(err)
-
-        log := Log{lines: lines}
-        logs[ident] = log
-      }
-      return nil
-  })
-  if err != nil {
-      panic(err)
-  }
-
-  return logs
 }
